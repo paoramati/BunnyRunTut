c79a3ca4a9c2726536379860abdf3ff7374f3a03
Brandon Paul <brandonmitchellpaul@gmail.com>
2017-07-21 23:26:24 +1200

    Finished [RNDBITS-009]
    A better bunny death and scoring
    "Select scene object and press f whilst hovered over scene to snap to that object"

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -2,33 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
-    private Animator myAnimator;
-
+    private Animator myAnim;
+    private Collider2D myCollider;
     public float bunnyJumpForce = 500f;
+    private float bunnyHurtTime = -1;
+    public Text scoreText;
+    private float startTime;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>();
+        myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonUp("Jump"))
+
+        if (bunnyHurtTime == -1)
         {
-            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            if (Input.GetButtonUp("Jump"))
+            {
+                myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            }
+
+            myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+
+            scoreText.text = (Time.time - startTime).ToString("0.0");
+        }
+        else
+        {
+            if (Time.time > bunnyHurtTime + 2) //i.e. 2 seconds after collision
+            {
+                SceneManager.LoadScene("Scene1");
+            }
         }
 
-        myAnimator.SetFloat("vVelocity", myRigidBody.velocity.y);
+		
 	}
 
+    //collision detection method
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy")) 
-            SceneManager.LoadScene("Scene1");        
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())  //disables newly spawned enemy from moving after collision
+            {
+                spawner.enabled = false;
+            }
+
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())          //disables enemy from moving after collision
+            {
+                moveLefter.enabled = false;
+            }
+
+            bunnyHurtTime = Time.time;
+            myAnim.SetBool("bunnyHurt", true);
+            myRigidBody.velocity = Vector2.zero;    //reset object velocity
+            myRigidBody.AddForce(transform.up * bunnyJumpForce);    //make bunny shoot up
+            myCollider.enabled = false;
+        }            
     }
 }
